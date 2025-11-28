6642633cdcf781f8efc1ea342e8588ea3fdb44d4
sase1 <saso_mitkovski@live.com>
2025-11-28 10:16:14 +0100

    listId page fix typeс

--- src/app/dashboard/list/[listId]/page.tsx
+++ src/app/dashboard/list/[listId]/page.tsx
@@ -1,5 +1,6 @@
 import ClientListPage from './ClientListPage';
 
-export default function Page({ params }: any) {
-    return <ClientListPage listId={params.listId} />;
+export default function Page(props: { params: { listId: string } }) {
+    const listId = props.params.listId;
+    return <ClientListPage listId={listId} />;
 }
