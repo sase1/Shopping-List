af8793c94eacd04bdac9322d7efb49440d92cb52
sase1 <saso_mitkovski@live.com>
2025-11-28 10:24:35 +0100

    listId page fix typeс

--- src/app/dashboard/list/[listId]/page.tsx
+++ src/app/dashboard/list/[listId]/page.tsx
@@ -1,6 +1,6 @@
 import ClientListPage from './ClientListPage';
 
-export default async function Page({ params }: { params: Record<string, string> }) {
-    // Do NOT define a separate interface for params
-    return <ClientListPage listId={params.listId} />;
+export default function Page(props: any) {
+    // Next.js passes params internally, we just forward them
+    return <ClientListPage listId={props.params.listId} />;
 }
